6c6fe9ade89c22d6dc44f106b991a8c78766cc2f
Neri <keshaeverywhere@gmail.com>
2017-07-28 22:53:37 +0300

    Add check and insert url function

--- db.py
+++ db.py
@@ -5,5 +5,16 @@ conn = sqlite3.connect('youtube.sqlite3')
 c = conn.cursor()
 
 def create_db():
-	c.execute('''CREATE TABLE videos
-              (id int primary key, name text, url text unique)''')
+	c.execute(
+		"CREATE TABLE videos (id integer primary key autoincrement, name text, url text unique)"
+	)
+
+def check_url(url):
+	c.execute(
+		"SELECT id FROM videos WHERE url = ?", url
+	)
+
+def insert_row(row):
+    c.execute(
+        "INSERT INTO videos VALUES (?,?,?)", row
+    )
