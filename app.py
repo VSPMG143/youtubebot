053327df89aa2b9ea3c411d996d6ae669236314b
Nerevarsoul <keshaeverywhere@gmail.com>
2017-11-12 18:07:13 +0300

    Redo bot with aio

--- app.py
+++ app.py
@@ -1,34 +1,37 @@
+import asyncio
 import time
 from urllib.parse import urlparse
 
-from telepot.loop import MessageLoop
+from telepot import glance
+from telepot.aio import Bot
+from telepot.aio.loop import MessageLoop
 
+from secret import TELEGRAM_TOKEN
 from db import check_url, insert_row
-from settings import *
 
 
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    logging.info((content_type, chat_type, chat_id))
+async def handle(msg):
+    content_type, chat_type, chat_id = glance(msg)
 
     if content_type == 'text':
-        process_message(msg['text'], chat_id)
+       await process_message(msg['text'], chat_id)
 
 
-def process_message(msg, chat_id):
+async def process_message(msg, chat_id):
     if urlparse(msg).netloc == 'www.youtube.com' and check_url(msg):
         insert_row((video.filename, msg))
         message = video.filename
-        logging.info('success!')
     else:
     	message = msg
-    bot.sendMessage(chat_id, message)
+    await bot.sendMessage(chat_id, message)
 
 
-logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL , filename=LOG_FILENAME)
+# logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL , filename=LOG_FILENAME)
 
 
-MessageLoop(bot, handle).run_as_thread()
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
+bot = Bot(TELEGRAM_TOKEN)
+loop.create_task(MessageLoop(bot, handle).run_forever())
+loop.run_forever()
 
-while 1:
-    time.sleep(10)
