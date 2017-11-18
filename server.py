e215437030ca734d77c343fbe98f2f774c337264
Nerevarsoul <keshaeverywhere@gmail.com>
2017-11-18 13:19:40 +0300

    refactor server

--- server.py
+++ server.py
@@ -2,30 +2,19 @@ import aiosqlite
 from aiohttp import web
 
 
-async def init_db(app):
-    async with aiosqlite.connect('youtube.sqlite3') as db:
-        cur = await db.cursor()
-        app['cursor'] = cur
-
-
-async def handle(request):
+async def get_new_videos(request):
     async with aiosqlite.connect('youtube.sqlite3') as db:
         cursor = await db.execute(
-            "SELECT * FROM videos"
+            "SELECT * FROM videos WHERE download=0"
         )
         videos = []
         async for row in cursor:
             videos.append(row)
     return web.json_response({'videos': videos})
 
-async def close_db(app):
-    await app['cursor'].close()
-
 
 app = web.Application()
-#app.on_startup.append(init_db)
-#app.on_cleanup.append(close_db)
-app.router.add_get('/', handle)
+app.router.add_get('/', get_new_videos)
 
 web.run_app(app, host='0.0.0.0', port=8080)
 
