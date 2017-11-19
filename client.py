59d0425a4d5cf95a9bae1692a36d02ed85a2e764
Neri <keshaeverywhere@gmail.com>
2017-11-19 20:25:26 +0300

    Done client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin
 
 import aiohttp
 import asyncio
@@ -12,11 +13,17 @@ async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get(SERVER_URL) as response:
             videos = await response.json()
-            for video in videos:
-                yt = YouTube(video[2])
-                yt_video = yt.get('mp4', '720p')
-                yt_video.download('/home/neri/downloads')    
-                session.post(SERVER_URL + 'update', json={'url'=video[2]})
+            for video in videos['videos']:
+                try:
+                    yt = YouTube(video[2])
+                    yt_video = yt.get('mp4', '720p')
+                    yt_video.download('/home/neri/downloads')  
+                    print('Success download!', video[2])  
+                except Exception as e:
+                    print(e, video[2])
+                finally:
+                    session.post(urljoin(SERVER_URL, 'update'), json={'url': video[2]})
+                    print('Success update!', video[2])
 
 
 loop = asyncio.get_event_loop()
