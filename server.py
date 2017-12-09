b5e53a5f5a27c7b58b039d687604b2940d821c51
Nerevarsoul <keshaeverywhere@gmail.com>
2017-12-09 18:17:32 +0300

    use postgres instead sqlite in server

--- server.py
+++ server.py
@@ -1,28 +1,40 @@
-import aiosqlite
+import aiopg
 from aiohttp import web
+
+from secret import DSN
+
+
+async def init_pg(app):
+    app['conn'] = await aiopg.connect(DSN)
+    return app
+
+
+async def close_pg(app):
+    app['conn'].close()
 
 
 async def get_new_videos(request):
-    async with aiosqlite.connect('youtube.sqlite3') as db:
-        cursor = await db.execute(
-            "SELECT * FROM videos WHERE download = 0"
-        )
-        videos = []
-        async for row in cursor:
-            videos.append(row)
+    cursor = await app['conn'].cursor()
+    await cursor.execute(
+        "SELECT * FROM videos WHERE download = false"
+    )
+    videos = []
+    async for row in cursor:
+        videos.append(row)
     return web.json_response({'videos': videos})
 
 
 async def update_video(request):
+    cursos = await app['db'].cursor()
     url = request.match_info.get('url', '')
-    print('Try to update: ', url)
-    async with aiosqlite.connect('youtube.sqlite3') as db: 
-        await db.execute('UPDATE videos SET download = 1 WHERE url = ?', (url,))
-        await db.commit()
+    await cursor.execute('UPDATE videos SET download = 1 WHERE url = (%s)', (url,))
+    await cursor.commit()
     return web.json_response({'status': 'ok'})
 
 
 app = web.Application()
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
 app.router.add_get('/', get_new_videos)
 app.router.add_post('/update', update_video)
 web.run_app(app, host='0.0.0.0', port=8080)
