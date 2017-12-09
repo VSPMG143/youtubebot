6c25b959595f9c95719f954d6b14f6dff2c50763
Nerevarsoul <keshaeverywhere@gmail.com>
2017-12-09 14:37:31 +0300

    Use postgres instead sqlite

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import asyncio
 import time
 from urllib.parse import urlparse
 
-import aiosqlite
+import aiopg
 from pytube import YouTube
 from telepot import glance
 from telepot.aio import Bot
 from telepot.aio.loop import MessageLoop
 
-from settings import DB_NAME
-from secret import TELEGRAM_TOKEN
+from secret import TELEGRAM_TOKEN, DSN
 
 
 async def handle(msg):
@@ -21,25 +20,22 @@ async def handle(msg):
 
 async def process_message(msg, chat_id):
     if urlparse(msg).netloc in ('www.youtube.com', 'youtu.be'):
-        async with aiosqlite.connect(DB_NAME) as db:
-            cursor = await db.execute("SELECT id FROM videos WHERE url = ?", (msg,))
-            if cursor.rowcount == -1:
-                try:
-                    yt = YouTube(msg)
-                    video = yt.get('mp4', '720p')
-                    await db.execute("INSERT INTO videos(name, url) VALUES (?,?)", (video.filename, msg))
-                    await db.commit()
-                    message = video.filename
-                except Exception as e:
-                    message = str(e)
-            else:
-                message = "This video is exist!"
+        async with aiopg.connect(DSN) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute('SELECT id FROM videos WHERE url = (%s)', (msg,))  
+                if cursor.rowcount == 0:
+                    try:
+                        yt = YouTube(msg)
+                        video = yt.get('mp4', '720p')
+                        await cursor.execute("INSERT INTO videos(name, url) VALUES (%s, %s)", (video.filename, msg))
+                        message = video.filename
+                    except Exception as e:
+                        message = str(e)
+                else:
+                    message = "This video is exist!"
     else:
     	message = msg
     await bot.sendMessage(chat_id, message)
-
-
-# logging.basicConfig(format=LOG_FORMAT, level=LOG_LEVEL , filename=LOG_FILENAME)
 
 
 loop = asyncio.get_event_loop()
