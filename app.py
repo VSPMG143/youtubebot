95d6b4d465a58b4887d50b2afd6252fad5be8b7f
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-01-18 08:56:30 +0300

    Add inline button for repeat download

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from pytube import YouTube
 from telepot import glance
 from telepot.aio import Bot
 from telepot.aio.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 from secret import TELEGRAM_TOKEN, DSN
 
@@ -32,15 +33,44 @@ async def process_message(msg, chat_id):
                     except Exception as e:
                         message = str(e)
                 else:
-                    message = "This video is exist!"
+                    message = 'This video is exist!'
+                    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                        [InlineKeyboardButton(text='Скачать еще раз?', callback_data=url)],
+                    ])
+
     else:
     	message = msg
+    await bot.sendMessage(chat_id, message)
+
+
+async def handle_callback(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+
+    await process_callback(query_data, from_id)
+
+
+async def process_callback(msg, chat_id):
+    if urlparse(msg).netloc in ('www.youtube.com', 'youtu.be'):
+        async with aiopg.connect(DSN) as conn:
+            async with conn.cursor() as cursor:
+                try:
+                    yt = YouTube(msg)
+                    video = yt.get('mp4', '720p')
+                    await cursor.execute('UPDATE videos SET download = false WHERE url = (%s)', (msg,))
+                    message = video.filename
+                except Exception as e:
+                    message = str(e)
+    else:
+        message = msg
     await bot.sendMessage(chat_id, message)
 
 
 loop = asyncio.get_event_loop()
 loop.set_debug(True)
 bot = Bot(TELEGRAM_TOKEN)
-loop.create_task(MessageLoop(bot, handle).run_forever())
+loop.create_task(MessageLoop(bot, 
+    {'chat': handle},
+    {'callback_query': handle_callback}
+).run_forever())
 loop.run_forever()
 
