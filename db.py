9cd81e8e8b5f40ac24691c1d91035095f71788b7
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2018-06-26 09:38:42 +0300

    add sqlalchemy table

--- db.py
+++ db.py
@@ -1,35 +1,12 @@
-import sqlite3
-from collections import namedtuple
-
-from settings import DB_NAME
+import sqlalchemy as sa
 
 
-conn = sqlite3.connect(DB_NAME)
-c = conn.cursor()
+metadata = sa.MetaData()
 
-
-def create_db():
-    c.execute(
-        "CREATE TABLE videos (id integer primary key autoincrement, name text, "
-        "url text unique, download boolean default 0, updated date default CURRENT_DATE)"
-    )
-
-
-def check_url(url):
-    c.execute(
-        "SELECT id FROM videos WHERE url = ?", (url,)
-    )
-    return c.rowcount == -1
-
-
-def insert_row(row):
-    c.execute(
-        "INSERT INTO videos(name, url) VALUES (?,?)", row
-    )
-    conn.commit()
-
-
-INSERT_ROW = "INSERT INTO videos(name, url) VALUES (?,?)"
-
-
-Video = namedtuple('Video', 'name url is_download id updated')
+videos = sa.Table(
+    'videos', metadata,
+    sa.Column('id', sa.Integer, primary_key=True),
+    sa.Column('name', sa.String(255)),
+    sa.Column('url', sa.String(255)),
+    sa.Column('download', sa.Boolean, default=False),
+)
