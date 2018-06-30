7106b912f60368bd0f2252721f40d4f3e4bc0265
Neri <keshaeverywhere@gmail.com>
2018-06-30 13:00:11 +0300

    Create class for akk case

--- app.py
+++ app.py
@@ -28,14 +28,23 @@ async def handle_message(msg):
 async def handle_callback(msg):
     query_id, from_id, query_data = glance(msg, flavor='callback_query')
     
-    path, msg = query_data.split(ProcessMessage.DIVIDER)
-    handle_class = ProcessMessage(msg, from_id)
-    await getattr(handle_class, path).start(force=True)
+    path, msg = query_data.split(BaseProcessMessage.DIVIDER)
+    if path == BaseProcessMessage.ONE_MORE:
+        handle_class = ProcessMessageReload(msg, from_id)
+    elif path == BaseProcessMessage.LOAD_LIST:
+        handle_class = ProcessMessageList(msg, from_id)
+    elif path == BaseProcessMessage.LOAD_ONE:
+        handle_class = ProcessMessageOne(msg, from_id)
+    else:
+        return
+    await handle_class.start()
 
 
-class ProcessMessage(object):
+class BaseProcessMessage(object):
     DIVIDER = '$#@'
     ONE_MORE = 'reload'
+    LOAD_LIST = 'list'
+    LOAD_ONE = 'one'
 
     def __init__(self, msg, chat_id):
         self.msg = msg
@@ -43,29 +52,8 @@ class ProcessMessage(object):
         self.keyboard = None
         self.videos = []
 
-    async def start(self, force=False):
-        if self.check_message():
-            async with aiopg.connect(DSN) as conn:
-                async with conn.cursor() as cursor:
-                    await cursor.execute('SELECT id FROM videos WHERE url = (%s)', (self.msg,))
-                    if await self.check_exist(cursor) or force:
-                        videos = self.get_videos()
-                        message = ''
-                        for video in videos:
-                            message += await self.load_video(cursor, video, force)
-                    else:
-                        message = 'This video is exist!'
-                        callback_data = f'{self.ONE_MORE}{self.DIVIDER}{self.msg}'
-                        self.keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                            [InlineKeyboardButton(text='Скачать еще раз?', callback_data=callback_data)],
-                        ])
-        else:
-            message = self.msg
-        if not self.keyboard:
-            self.keyboard = ReplyKeyboardMarkup(keyboard=[
-                 [KeyboardButton(text='/look_videos')],
-            ])
-        await bot.sendMessage(self.chat_id, message, reply_markup=self.keyboard)
+    def check_message(self):
+        return urlparse(self.msg).netloc in ('www.youtube.com', 'youtu.be')
 
     def get_videos(self):
         try:
@@ -76,21 +64,35 @@ class ProcessMessage(object):
             videos = [self.msg]
         return videos
 
-    async def load_video(self, cursor, video, force):
+    def get_stream(self, yt):
+        streams = yt.streams.filter(file_extension='mp4', type='video').order_by('resolution').desc().all()
+        for stream in streams:
+            if stream.includes_audio_track and stream.resolution <= '720p':
+                return stream
+        return yt.streams.first()
+
+    async def start(self):
+        if self.check_message():
+            await self.process_message()
+        else:
+            message = self.msg
+            await bot.sendMessage(self.chat_id, message, reply_markup=self.keyboard)
+
+    async def process_message(self):
+        pass
+
+    async def send_message(self, message):
+        await bot.sendMessage(self.chat_id, message, reply_markup=self.keyboard)
+
+    async def load_video(self, cursor, video):
         try:
             stream = self.fetch_stream(video)
-            if force:
-                await cursor.execute('UPDATE videos SET download = false WHERE url = (%s)', (video,))
-            else:
-                await cursor.execute(
-                    'INSERT INTO videos(name, url) VALUES (%s, %s)', (stream.default_filename, video)
-                )
+            await cursor.execute(
+                'INSERT INTO videos(name, url) VALUES (%s, %s)', (stream.default_filename, video)
+            )
             return stream.default_filename
         except Exception as e:
             return str(e)
-
-    def check_message(self):
-        return urlparse(self.msg).netloc in ('www.youtube.com', 'youtu.be')
 
     @staticmethod
     def fetch_stream(video):
@@ -101,13 +103,66 @@ class ProcessMessage(object):
         await cursor.execute('SELECT id FROM videos WHERE url = (%s)', (self.msg,))
         return cursor.rowcount == 0
 
-    async def reload(self):
+
+class ProcessMessage(BaseProcessMessage):
+
+    async def process_message(self):
         async with aiopg.connect(DSN) as conn:
             async with conn.cursor() as cursor:
-                videos = self.get_videos()
-                message = ''
-                for video in videos:
-                    message += await self.load_video(cursor, video, True)
+                await cursor.execute('SELECT id FROM videos WHERE url = (%s)', (self.msg,))
+                if await self.check_exist(cursor):
+                    videos = self.get_videos()
+                    if isinstance(videos, list):
+                        message = 'Will you want load playlist?'
+                        callback_data = f'{self.LOAD_LIST}{self.DIVIDER}{self.msg}'
+                        callback_data2 = f'{self.LOAD_ONE}{self.DIVIDER}{self.msg}'
+                        self.keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                            [InlineKeyboardButton(text='Скачать весь плэйлист?', callback_data=callback_data)],
+                            [InlineKeyboardButton(text='Скачать только это видео?', callback_data=callback_data2)]
+                        ])
+                    else:
+                        message = await self.load_video(cursor, videos)
+                else:
+                    message = 'This video is exist!'
+                    callback_data = f'{self.ONE_MORE}{self.DIVIDER}{self.msg}'
+                    self.keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                        [InlineKeyboardButton(text='Скачать еще раз?', callback_data=callback_data)],
+                    ])
+                await self.send_message(message)
+
+
+class ProcessMessageReload(BaseProcessMessage):
+
+    async def process_message(self):
+        async with aiopg.connect(DSN) as conn:
+            async with conn.cursor() as cursor:
+                message = await self.load_video(cursor, self.msg)
+                await self.send_message(message)
+
+    async def load_video(self, cursor, video,):
+        try:
+            stream = self.fetch_stream(video)
+            await cursor.execute('UPDATE videos SET download = false WHERE url = (%s)', (video,))
+            return stream.default_filename
+        except Exception as e:
+            return str(e)
+
+
+class ProcessMessageList(BaseProcessMessage):
+    async with aiopg.connect(DSN) as conn:
+        async with conn.cursor() as cursor:
+            videos = self.get_videos()
+            for video in videos:
+                message = await self.load_video(cursor, video)
+                await self.send_message(message)
+
+
+class ProcessMessageOne(BaseProcessMessage):
+    async def process_message(self):
+        async with aiopg.connect(DSN) as conn:
+            async with conn.cursor() as cursor:
+                message = await self.load_video(cursor, self.msg)
+                await self.send_message(message)
 
 
 if __name__ == '__main__':
--- utils.py
+++
@@ -1,6 +0,0 @@
-def get_stream(yt):
-    streams = yt.streams.filter(file_extension='mp4', type='video').order_by('resolution').desc().all()
-    for stream in streams:
-        if stream.includes_audio_track and stream.resolution <= '720p':
-            return stream
-    return yt.streams.first()
