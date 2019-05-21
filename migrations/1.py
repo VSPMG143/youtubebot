aaa658dbb02e6cd73c17628a66f161bc09997dfe
Innokentiy <keshaeverywhere@gmail.com>
2019-05-21 14:31:54 +0300

    up version aiopg to 0.16

--- migrations/1.py
+++ migrations/1.py
@@ -3,10 +3,10 @@ from db import conn
 
 c = conn.cursor()
 
+
 def add_date_column():
     c.execute("alter table videos add column 'create_at' 'datetime default current_timestamp'")
   
   
 if __name__ == "__main__":
     add_date_column()
-  
